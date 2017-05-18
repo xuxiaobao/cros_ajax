a00f180dbcf74ea7997db37bd66e54f0286e82f3
xuxiaobao <2269649004@qq.com>
2017-05-19 07:22:14 +0800

    简单定时任务使用

--- src/main/java/com/cros/service/HelloService.java
+++ src/main/java/com/cros/service/HelloService.java
@@ -8,9 +8,41 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class HelloService {
-
-    @Scheduled(fixedDelay = 2000)
+    /**
+     * 上次任务执行结束时间到下次任务开始时间，间隔是2000毫秒
+     */
+    /*@Scheduled(fixedDelay = 2000)
     public void task() {
         System.out.println("schedule start!");
+    }*/
+
+    /**
+     * 上次任务开始时间到下次任务开始时间，间隔是2000毫秒
+     */
+    /*@Scheduled(fixedRate = 2000)
+    public void task() {
+        System.out.println("schedule start");
+    }*/
+
+    /**
+     * 详细配置方法执行
+     * 秒 分 时 日 月 周
+     *
+     * 下面配置2秒执行一次
+     */
+    /**
+     *
+     字段         允许值                 允许的特殊字符
+     秒 	  	     0-59 	  	            , - * /
+     分 	  	     0-59 	  	            , - * /
+     小时 	  	 0-23 	  	            , - * /
+     日期 	  	 1-31 	  	            , - * ? / L W C
+     月份 	   	 1-12 或者 JAN-DEC 	  	, - * /
+     星期 	  	 1-7 或者 SUN-SAT 	  	, - * ? / L C #
+     年（可选） 	 留空, 1970-2099 	  	, - * /
+     */
+    @Scheduled(cron = "0-6/2 * * * * ?")
+    public void task() {
+        System.out.println("cron schedule start");
     }
 }
