72959532fc75b9a7230ae3ff155a24779fbcaa21
xuxiaobao <2269649004@qq.com>
2017-05-19 08:21:14 +0800

    fastjson解析

---
+++ src/main/java/com/cros/pojo/Person.java
@@ -0,0 +1,6 @@
+package com.cros.pojo;
+
+/**
+ * Created by Administrator on 2017/5/19.
+ */
+
--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,17 +1,24 @@
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+
+import java.util.Map;
+
 /**
  * Created by Administrator on 2017/5/18.
  */
 public class Test {
     public static void main(String[] args) {
-        try {
-            int a = 1/0;
-        } catch (Exception e) {
-            System.out.println("exception");
-            return;
-        } finally {
-            System.out.println("end");
-        }
+        fastJson();
+    }
 
-        System.out.println("other");
+    /**
+     * Json序列化和反序列化
+     */
+    public static void fastJson() {
+        String json = "{'person':{'name':'xxb'}}";
+        JSONObject object = JSON.parseObject(json);
+        for (Map.Entry<String, Object> entry : object.entrySet()) {
+            System.out.println(entry.getKey() + ":" + entry.getValue());
+        }
     }
 }
